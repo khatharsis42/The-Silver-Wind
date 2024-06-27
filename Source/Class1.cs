53dddcaa3fdbb15fc92c2ff4de9b4d8091e8bad0
Catherine Guelque <guelque@telecom-sudparis.eu>
2024-06-27 09:16:19 +0200

    Added source code

---
+++ Source/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOR_Mawan_Mod
+{
+    public class PassiveAbility_10997119000: PassiveAbilityBase
+    {
+
+    }
+}
