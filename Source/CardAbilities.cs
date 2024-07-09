f9bbc874dd583eca70adf8b8c02c1e95316cf01b
Catherine Guelque <guelque@telecom-sudparis.eu>
2024-07-09 22:51:21 +0200

    Added some keywords and started working on Retention

--- Source/CardAbilities.cs
+++ Source/CardAbilities.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace LOR_Mawan_Mod
 {
@@ -18,61 +19,73 @@ namespace LOR_Mawan_Mod
             harmony.PatchAll();
             Harmony.DEBUG = true;
         }
+
+        /** Returns a random values from this list. */
+        public static T getRandomValues<T>(List<T> list)
+        {
+            var random = new System.Random();
+            var index = random.Next(list.Count);
+            return list[index];
+        }
     }
 
     public class DiceCardSelfAbility_energy1ally : DiceCardSelfAbilityBase
     {
         public static string Desc = "[On Use]: Restore 1 Light to a random ally.";
+        public new string[] Keywords = { "Energy_Keyword" };
         public override void OnUseCard()
         {
-            var random = new Random();
+            var random = new UnityEngine.Random();
             List<BattleUnitModel> aliveUnits = BattleObjectManager.instance.GetAliveList(this.owner.faction);
-            int index = 0;
-            if (aliveUnits.Count > 1) do
-                {
-                    index = random.Next(aliveUnits.Count);
-                } while (aliveUnits[index] == this.owner);
-            aliveUnits[index].cardSlotDetail.RecoverPlayPoint(1);
-            FileLog.Log("Using ability: energy1ally on " + index);
+            aliveUnits.Remove(this.owner);
+            if (aliveUnits.Count > 1)
+                LOR_Mawan_Mod.getRandomValues(aliveUnits).cardSlotDetail.RecoverPlayPointByCard(1);
+            else
+                owner.cardSlotDetail.RecoverPlayPointByCard(1);
         }
     }
 
     public class DiceCardAbility_energy2atk : DiceCardAbilityBase
     {
         public static string Desc = "[On Hit] Restore 2 Light.";
+        public new string[] Keywords = { "Energy_Keyword" };
         public override void OnSucceedAttack()
         {
             FileLog.Log("Using ability: energy2atk");
-            this.owner.cardSlotDetail.RecoverPlayPoint(2);
+            this.owner.cardSlotDetail.RecoverPlayPointByCard(2);
         }
     }
     public class DiceCardSelfAbility_energy1ally2 : DiceCardSelfAbilityBase
     {
         public static string Desc = "[On Use]: Restore 2 Light to 2 random allies.";
+        public new string[] Keywords = { "Energy_Keyword" };
         public override void OnUseCard()
         {
-            var random = new Random();
             List<BattleUnitModel> aliveUnits = BattleObjectManager.instance.GetAliveList(this.owner.faction);
-            int index = 0;
-            if (aliveUnits.Count > 1) do
-                {
-                    index = random.Next(aliveUnits.Count);
-                } while (aliveUnits[index] == this.owner);
-            aliveUnits[index].cardSlotDetail.RecoverPlayPoint(1);
-            aliveUnits.Remove(aliveUnits[index]);
-            FileLog.Log("Using ability: energy1ally2 on " + index);
-            index = 0;
-            if (aliveUnits.Count > 1) do
-                {
-                    index = random.Next(aliveUnits.Count);
-                } while (aliveUnits[index] == this.owner);
-            aliveUnits[index].cardSlotDetail.RecoverPlayPoint(1);
-            FileLog.Log("Using ability: energy1ally2 on " + index);
+            aliveUnits.Remove(this.owner);
+            if (aliveUnits.Count >= 2)
+            {
+                var firstWinner = LOR_Mawan_Mod.getRandomValues(aliveUnits);
+                aliveUnits.Remove(firstWinner);
+                var secondWinner = LOR_Mawan_Mod.getRandomValues(aliveUnits);
+                firstWinner.cardSlotDetail.RecoverPlayPointByCard(2);
+                secondWinner.cardSlotDetail.RecoverPlayPointByCard(2);
+                return;
+            }
+            if (aliveUnits.Count == 1)
+            {
+                aliveUnits.First().cardSlotDetail.RecoverPlayPointByCard(2);
+                owner.cardSlotDetail.RecoverPlayPointByCard(2);
+                return;
+            }
+            owner.cardSlotDetail.RecoverPlayPointByCard(4);
         }
     }
     public class DiceCardAbility_clashdraw1_max1 : DiceCardAbilityBase
     {
         public static string Desc = "[On Clash Win] Draw 1 Page (1 time).";
+        public new string[] Keywords = { "DrawCard_Keyword" };
+        
         bool hasAlreadyRolled = false;
         public override void OnWinParryingDefense()
         {
@@ -85,6 +98,52 @@ namespace LOR_Mawan_Mod
             FileLog.Log("Can't use ability: clashdraw1_max1");
                 hasAlreadyRolled = true;
             }
+        }
+    }
+    public class DiceCardSelfAbility_energy1ally2_draw1ally : DiceCardSelfAbility_energy1ally2
+    {
+        public static string Desc = "[On Use]: Restore 2 Light to 2 random allies. A random ally draws a page.";
+        public new string[] Keywords = { "DrawCard_Keyword", "Energy_Keyword" };
+        public override void OnUseCard()
+        {
+            base.OnUseCard();
+            List<BattleUnitModel> aliveUnits = BattleObjectManager.instance.GetAliveList(this.owner.faction);
+            aliveUnits.Remove(this.owner);
+            if (aliveUnits.Count >= 1)
+                LOR_Mawan_Mod.getRandomValues(aliveUnits).allyCardDetail.DrawCards(1);
+            else
+                owner.allyCardDetail.DrawCards(1);
+        }
+    }
+
+    public class BattleDiceCardBuf_retention : BattleUnitBuf
+    {
+        private BattleDiceCardModel cardToDraw = null;
+        public override void OnUseCard(BattlePlayingCardDataInUnitModel card)
+        {
+            base.OnUseCard(card);
+            if (cardToDraw != null || card.card.XmlData.IsEgo() || card.card.XmlData.IsPersonal() || card.card.XmlData.IsExhaustOnUse())
+                return;
+            cardToDraw = card.card;
+        }
+        public override void OnRoundEnd()
+        {
+            base.OnRoundEnd();
+            _owner.allyCardDetail.AddCardToHand(cardToDraw);
+            // FIXME This creates copy of our card, I think ?
+        }
+    }
+
+    public class DiceCardSelfAbility_retention : DiceCardSelfAbilityBase
+    {
+        public static string Desc = "[On Use]: Return the first combat page played this scene to hand at the end of the scene.\r\n" +
+            "(Excluding On-play, Single-Use, EGO and character-specific pages).";
+        public new string[] Keywords = { "Instant_Keyword" };
+        public override void OnUseInstance(BattleUnitModel unit, BattleDiceCardModel self, BattleUnitModel targetUnit)
+        {
+            base.OnUseInstance(unit, self, targetUnit);
+            var buf = new BattleDiceCardBuf_retention();
+            unit.bufListDetail.AddBuf(buf);
         }
     }
 }
